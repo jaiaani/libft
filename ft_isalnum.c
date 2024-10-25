d0c55aea118a690cd665d4b9b35584b13bc34eaf
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2024-10-25 11:50:21 -0300

    clean ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,11 +6,12 @@
 /*   By: jados-sa <jados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 21:16:44 by jados-sa          #+#    #+#             */
-/*   Updated: 2024/10/23 21:16:49 by jados-sa         ###   ########.fr       */
+/*   Updated: 2024/10/25 11:49:14 by jaiane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Checks for an alphanumeric character 				      */
+
 #include "libft.h"
 
 int	ft_isalnum(char c)
