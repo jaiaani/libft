33a5cef4035cd4194bb308fd71d240fb9ddbb22c
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2024-10-25 13:13:19 -0300

    clean memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jados-sa <jados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 18:18:46 by jados-sa          #+#    #+#             */
-/*   Updated: 2024/10/23 21:17:39 by jados-sa         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:12:54 by jaiane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
 	unsigned char	*d;
 	unsigned char	*s;
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
-	if (!dest && !src)
+	if (!dest || !src)
 		return (NULL);
 	if (dest != src)
 	{
@@ -35,16 +35,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-int main ()
-{
-   const char src[19] = "Copy me as a memory";
-   char dest[19];
-   strcpy(dest,"xxxx if you can");
-   printf("Before ft_memcpy dest = %s\n", dest);
-   ft_memcpy(dest, src, 24);
-   printf("After ft_memcpy dest = %s\n", dest);
-   return(0);
-}*/
