45497a3b5489ab2ced42e59e081568dbc3526905
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2024-10-25 13:06:39 -0300

    clean ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,20 @@
 /*   By: jaiane <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:24:58 by jaiane            #+#    #+#             */
-/*   Updated: 2024/10/23 10:49:37 by jaiane           ###   ########.fr       */
+/*   Updated: 2024/10/25 13:06:01 by jaiane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Compare memory areas							      *
  * Compares the first n bytes (each interpreted as unsigned char) of 	      *
- * the memory s1 and s2							      */
+ * the memory s1 and s2							      
+ * Returns an integer less than, equal to, or greater than zero if the 
+ *  first n bytes of s1 is found, respectively, to be less than, to match, 
+ *  or be greater than the first n bytes of s2.
+ * For a nonzero return value, the sign is determined by the sign of the 
+ * 	difference between the first pair of bytes (interpreted as unsigned char)
+ * 	that differ in s1 and s2.
+ * If n is zero, the return value is zero.*/
 
 #include "libft.h"
 
@@ -35,38 +42,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
-/*
-#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-    char str1[] = "Hello, World!";
-    char str2[] = "Hello, World!";
-    char str3[] = "Hello, Earth!";
-    char str4[] = "Hello";
-    char str5[] = "Hellz";
-
-    int result = ft_memcmp(str1, str2, strlen(str1));
-    printf("Teste 1: memcmp(str1, str2) = %d\n", result);
-
-    result = ft_memcmp(str1, str3, strlen(str1));
-    printf("Teste 2: memcmp(str1, str3) = %d\n", result); 
-
-    result = ft_memcmp(str1, str4, 5);
-    printf("Teste 3: memcmp(str1, str4, 5) = %d\n", result);
-
-    result = ft_memcmp(str4, str5, 5);
-    printf("Teste 4: memcmp(str4, str5, 5) = %d\n", result); 
-
-    unsigned char mem1[] = {0x01, 0x02, 0x03, 0x04, 0x05};
-    unsigned char mem2[] = {0x01, 0x02, 0x03, 0x04, 0x06};
-
-    result = ft_memcmp(mem1, mem2, sizeof(mem1));
-    printf("Teste 5: memcmp(mem1, mem2) = %d\n", result);  // (0x05 < 0x06)
-
-    result = ft_memcmp(mem1, mem1, sizeof(mem1));
-    printf("Teste 6: memcmp(mem1, mem1) = %d\n", result);  // 0
-
-    return 0;
-}*/
