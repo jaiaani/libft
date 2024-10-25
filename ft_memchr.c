c5b70f1be1b99a25b6651cbadea272155a342546
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2024-10-25 13:00:13 -0300

    clean ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jaiane <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 20:14:37 by jaiane            #+#    #+#             */
-/*   Updated: 2024/10/23 20:44:59 by jados-sa         ###   ########.fr       */
+/*   Updated: 2024/10/25 12:59:47 by jaiane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,27 +34,3 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
-/*
-#include <stdio.h>
-#include <memory.h>
-
-int main( void )
-{
-	int  ch = 'r';
-	char string[] = "The quick brown dog jumps over the lazy fox";
-	char fmt1[] =   "         1         2         3         4         5";
-	char fmt2[] =   "12345678901234567890123456789012345678901234567890";
-
-   	char *pdest;
-   	int result;
-   	printf( "String to be searched:\n             %s\n", string );
-   	printf( "             %s\n             %s\n\n", fmt1, fmt2 );
-
-   	printf( "Search char: %c\n", ch );
-   	pdest = ft_memchr(string, ch, sizeof( string ) );
-   	result = (int)(pdest - string + 1);
-   	if ( pdest != NULL )
-      		printf( "Result:      %c found at position %d\n", ch, result);
-   	else
-     		printf( "Result:      %c not found\n", ch );
-}*/
